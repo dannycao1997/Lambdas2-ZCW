ac8315fb2055867e6efe98528432283170389827
Danny Cao <dannycao1997@yahoo.com>
2024-04-09 22:58:20 -0400

    created constructor for person class

--- src/main/java/rocks/zipcode/lambdas/Person.java
+++ src/main/java/rocks/zipcode/lambdas/Person.java
@@ -1,5 +1,7 @@
 package main.java.rocks.zipcode.lambdas;
 
+import java.time.LocalDate;
+
 public class Person {
 
     public enum Sex {
@@ -10,6 +12,33 @@ public class Person {
     LocalDate birthday;
     Sex gender;
     String emailAddress;
+
+    public Person(String name, LocalDate birthday, Sex gender, String emailAddress){
+        this.name = name;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.emailAddress = emailAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+
+
+
 
     public int getAge() {
         // ...
